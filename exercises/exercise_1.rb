247882c96f2644e2e4c43c4f9360f268743a0440
Sidney Cardozo <scooby123sid@gmail.com>
2017-08-01 16:34:59 +0000

    Exercise 1 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,20 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+s1 = Store.create( name: "Burnaby",
+                   annual_revenue: 300000,
+                   mens_apparel: true,
+                   womens_apparel: true
+  )
+
+s2 = Store.create( name: "Richmond",
+                   annual_revenue: 1260000,
+                   womens_apparel: true
+  )
+
+s3 = Store.create( name: "Gastown",
+                   annual_revenue: 100000,
+                   mens_apparel: true
+  )
+
+puts Store.count
